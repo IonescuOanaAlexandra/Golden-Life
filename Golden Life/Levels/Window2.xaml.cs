1cc85ab026aee69dc75487ee4f0de8d9f2240efe
MocanuAlice <alicemocanu06@gmail.com>
2023-04-04 13:22:32 +0300

    Animatie1

--- Golden Life/Levels/Window2.xaml.cs
+++ Golden Life/Levels/Window2.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using Path = System.IO.Path;
@@ -32,26 +33,6 @@ namespace Golden_Life
 
         }
 
-        private void Grid_KeyDown(object sender, KeyEventArgs e)
-        {
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-
-            if (Keyboard.IsKeyDown(Key.S) && AnimationPlaying == false)
-            {
-                MePlayer.Source = new System.Uri(baseDirectory + "\\Animations\\egypt_center.mov");
-                AnimationPlaying = true;
-            }
-            if (Keyboard.IsKeyDown(Key.D) && AnimationPlaying == false)
-            {
-                MePlayer.Source = new System.Uri(baseDirectory + "\\Animations\\egypt_right.mov");
-                AnimationPlaying = true;
-            }
-            if (Keyboard.IsKeyDown(Key.A) && AnimationPlaying == false)
-            {
-                MePlayer.Source = new System.Uri(baseDirectory + "\\Animations\\egypt_left.mov");
-                AnimationPlaying = true;
-            }
-        }
 
         private void MePlayer_Initialized(object sender, EventArgs e)
         {
@@ -65,6 +46,27 @@ namespace Golden_Life
         private void MePlayer_MediaOpened(object sender, RoutedEventArgs e)
         {
             MePlayer.Play();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (Keyboard.IsKeyDown(Key.S) && AnimationPlaying == false)
+            {
+                MePlayer.Source = new System.Uri(baseDirectory + "\\Animations\\egypt.center.mov");
+                AnimationPlaying = true;
+            }
+            if (Keyboard.IsKeyDown(Key.D) && AnimationPlaying == false)
+            {
+                MePlayer.Source = new System.Uri(baseDirectory + "\\Animations\\egypt.right.mov");
+                AnimationPlaying = true;
+            }
+            if (Keyboard.IsKeyDown(Key.A) && AnimationPlaying == false)
+            {
+                MePlayer.Source = new System.Uri(baseDirectory + "\\Animations\\egypt.left.mov");
+                AnimationPlaying = true;
+            }
         }
     }
 }
